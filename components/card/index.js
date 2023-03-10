464f8985f207d53ba0c538cb815316ea594535ae
And4Web <anands2807@gmail.com>
2023-03-10 09:37:31 +0530

    cards with different sizes created.

--- components/card/index.js
+++ components/card/index.js
@@ -1,22 +1,32 @@
-import Image from 'next/image';
-import styles from './card.module.css';
+import Image from "next/image";
+import styles from "./card.module.css";
 
 function Card(props) {
-  const {imgUrl, size} = props;
+  const { imgUrl, size } = props;
 
   const classMap = {
-    large: "styles.lgItem",
-    medium: "styles.mdIem",
-    small: "styles.smItem"
-  }
+    large: styles.lgItem,
+    medium: styles.mdItem,
+    small: styles.smItem,
+  };
+
+  // console.log("classMap: ", classMap[size]);
 
   return (
     <div>
-      <div className={classMap[size]}>
-        <Image src={imgUrl} alt='card image'  height="200" width="200"/>
+      <div className={styles.container}>
+        {`Card: ${size}`}
+        <div className={classMap[size]}>
+          <Image
+            src={imgUrl}
+            alt={`card image: ${size}`}
+            fill
+            className={styles.cardImg}
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
