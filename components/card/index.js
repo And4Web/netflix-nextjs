9677bfd317cd38bd484d4953f085f3cdca4e7a2f
And4Web <anands2807@gmail.com>
2023-03-10 12:10:59 +0530

    cards done with framer gestures for hover effect

--- components/card/index.js
+++ components/card/index.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 import Image from "next/image";
 import styles from "./card.module.css";
 
+import cls from 'classnames'
+import {motion} from 'framer-motion'
+
 function Card(props) {
   const { imgUrl, size } = props;
   const [imgSrc, setImgSrc] = useState(imgUrl);
@@ -13,14 +16,17 @@ function Card(props) {
   };
 
   const handleOnError = () => {
-    setImgSrc("/static/banner.jpeg")
+    setImgSrc("https://images.unsplash.com/photo-1440404653325-ab127d49abc1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")
   }
 
   return (
-    <div>
+    
       <div className={styles.container}>
         {`Card: ${size}`}
-        <div className={classMap[size]}>
+        <motion.div 
+        whileHover={{scale: 1.2, transition: {duration: 0.5}}}
+        whileTap={{scale: 0.9}}
+        className={cls(styles.imgMotionWrapper, classMap[size])}>
           <Image
             src={imgSrc}
             alt={`card image: ${size}`}
@@ -28,9 +34,9 @@ function Card(props) {
             onError={handleOnError}
             className={styles.cardImg}
           />
-        </div>
+        </motion.div>
       </div>
-    </div>
+    
   );
 }
 
