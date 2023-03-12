d91e91545b06dbe83f9c2b028886823165f57f91
And4Web <anands2807@gmail.com>
2023-03-12 15:31:43 +0530

    error handelled for youtube api fetch

--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,21 @@ import Card from "@/components/card/card";
 import SectionCard from "@/components/card/section-card";
 
 export async function getServerSideProps(){
-  const disneyVideos = await getVideos();
+  const disneyVideos = await getVideos("disney trailers");
+  const popularVideos = await getVideos("most popular");
+  const premiumVideos = await getVideos("premium");
+  const productivityVideos = await getVideos("productivity");
+  const adventureVideos = await getVideos("adventure");
 
   // console.log("<===disneyVideos most recent:===> ", disneyVideos);
-  
+
   return {
-    props: {disneyVideos}
+    props: {disneyVideos, popularVideos, premiumVideos, productivityVideos, adventureVideos}
   }
 }
 
 export default function Home(props) {
-  const {disneyVideos} = props;  
+  const {disneyVideos, popularVideos, premiumVideos, adventureVideos, productivityVideos} = props;  
 
   return (
     <>
@@ -50,8 +54,10 @@ export default function Home(props) {
       {/* Cards */}
       <div className={styles.sectionWrapper}>
         <SectionCard title="Disney" videos={disneyVideos} size="large"/>
-        <SectionCard title="Most popular" videos={disneyVideos} size="small"/>
-        <SectionCard title="Premium" videos={disneyVideos} size="medium"/>
+        <SectionCard title="Most popular" videos={popularVideos} size="small"/>
+        <SectionCard title="Premium" videos={premiumVideos} size="medium"/>
+        <SectionCard title="Productivity" videos={productivityVideos} size="large"/>
+        <SectionCard title="Adventure" videos={adventureVideos} size="small"/>
       </div>
     </>
   );
