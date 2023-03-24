356091db439f3ff075b91e9dee49f77cf16f3825
And4Web <anands2807@gmail.com>
2023-03-24 14:12:29 +0530

    app route once logged in

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,23 @@
+import {useEffect} from 'react';
+import {magic} from '../lib/magic-client';
+import {useRouter} from 'next/router';
+
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(()=>{
+    const effect = async ()=>{
+      const isLoggedIn = await magic.user.isLoggedIn();
+      if(isLoggedIn){
+        router.push("/");
+      }else{
+        router.push("/login");
+      }
+    }
+    effect();
+  }, [])
   return <Component {...pageProps} />
 }
