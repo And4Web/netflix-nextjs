74b34767d322a37e94e824f88a84f48bd0721a72
And4Web <anands2807@gmail.com>
2023-03-24 14:46:06 +0530

    created and implemented a new loading component to show isLoading state

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,40 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {magic} from '../lib/magic-client';
 import {useRouter} from 'next/router';
+
+import Loading from '@/components/loading/loading';
 
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-
+  const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
 
   useEffect(()=>{
     const effect = async ()=>{
       const isLoggedIn = await magic.user.isLoggedIn();
       if(isLoggedIn){
+        // setIsLoading(false)
         router.push("/");
       }else{
+        // setIsLoading(false)
         router.push("/login");
       }
     }
     effect();
+  }, [])  
+
+  useEffect(()=>{
+    const handleRouterChange = () => {
+      setIsLoading(false)
+    }    
+    router.events.on('routeChangeComplete', handleRouterChange);
+    router.events.on('routeChangeError', handleRouterChange);
+    return ()=>{
+      router.events.off('routeChangeComplete', handleRouterChange);
+      router.events.off('routeChangeError', handleRouterChange);
+    }
   }, [])
-  return <Component {...pageProps} />
+
+  return isLoading? <Loading/>: <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -12,22 +12,32 @@ import NavBar from "@/components/navbar";
 import Card from "@/components/card/card";
 import SectionCard from "@/components/card/section-card";
 
-export async function getServerSideProps(){
+export async function getServerSideProps() {
   const disneyVideos = await getVideos("disney trailers");
   const popularVideos = await getPopularVideos();
   const premiumVideos = await getVideos("premium");
   const productivityVideos = await getVideos("productivity");
   const adventureVideos = await getVideos("adventure");
 
-  // console.log("<===disneyVideos most recent:===> ", disneyVideos);
-
   return {
-    props: {disneyVideos, popularVideos, premiumVideos, productivityVideos, adventureVideos}
-  }
+    props: {
+      disneyVideos,
+      popularVideos,
+      premiumVideos,
+      productivityVideos,
+      adventureVideos,
+    },
+  };
 }
 
 export default function Home(props) {
-  const {disneyVideos, popularVideos, premiumVideos, adventureVideos, productivityVideos} = props;  
+  const {
+    disneyVideos,
+    popularVideos,
+    premiumVideos,
+    adventureVideos,
+    productivityVideos,
+  } = props;
 
   return (
     <>
@@ -40,27 +50,38 @@ export default function Home(props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-<div className={styles.main}>
-     {/* Navbar */}
-     <NavBar username="anand@gmail.com" />
+      <div className={styles.main}>
+        {/* Navbar */}
+        <NavBar username="anand@gmail.com" />
 
-{/* Banner */}
-<Banner
-  title="Movie title"
-  subtitle="very ordinary subtitle"
-  imgUrl="/static/banner.jpeg"
-/>
+        {/* Banner */}
+        <Banner
+          title="Movie title"
+          subtitle="very ordinary subtitle"
+          imgUrl="/static/banner.jpeg"
+        />
 
-{/* Cards */}
-<div className={styles.sectionWrapper}>
-  <SectionCard title="Disney" videos={disneyVideos} size="large"/>
-  <SectionCard title="Most popular" videos={popularVideos} size="small"/>
-  <SectionCard title="Premium" videos={premiumVideos} size="medium"/>
-  <SectionCard title="Productivity" videos={productivityVideos} size="large"/>
-  <SectionCard title="Adventure" videos={adventureVideos} size="small"/>
-</div>
-
-</div>
-     </>
+        {/* Cards */}
+        <div className={styles.sectionWrapper}>
+          <SectionCard title="Disney" videos={disneyVideos} size="large" />
+          <SectionCard
+            title="Most popular"
+            videos={popularVideos}
+            size="small"
+          />
+          <SectionCard title="Premium" videos={premiumVideos} size="medium" />
+          <SectionCard
+            title="Productivity"
+            videos={productivityVideos}
+            size="large"
+          />
+          <SectionCard
+            title="Adventure"
+            videos={adventureVideos}
+            size="small"
+          />
+        </div>
+      </div>
+    </>
   );
 }
