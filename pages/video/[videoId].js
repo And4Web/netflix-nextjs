c37393b9a96ff671f04f5be6e1ea9b796a2c3940
And4Web <anands2807@gmail.com>
2023-03-26 09:22:36 +0530

    react-modal installed to create video modal page

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,11 +1,20 @@
-import {useRouter} from 'next/router';
+import { useRouter } from "next/router";
+import Modal from "react-modal";
+import styles from '../../styles/video.module.css';
+
+Modal.setAppElement("#__next");
 
 function Video() {
   const router = useRouter();
-  
+
   return (
-    <div>Some Video: {router.query.videoId}</div>
-  )
+    <div>
+      Some Video: {router.query.videoId}
+      <Modal isOpen={true} contentLabel="Watch the video" onRequestClose={()=>{router.back()}} overlayClassName={styles.overlay}>
+        <div>Modal body</div>
+      </Modal>
+    </div>
+  );
 }
 
-export default Video
+export default Video;
